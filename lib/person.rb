3dceb0d7a99a9e23ee14fc3289e689e5d3351520
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-21 19:51:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,4 +8,7 @@ class Person
 
 end
 
-# initialize takes in some unspecified attributes object. Then, we iterate over each key/value pair in the attributes hash. The name of the key becomes the name of a setter method and the value associated with the key is the name of the value you want to pass to that method. The ruby .send method then calls the method name that is the key’s name, with an argument of the value.
+# initialize takes in some unspecified attributes object
+# then, wrist_sizee iterate over each key/value pair in the attributes hash
+# the name of the key becomes the name of a setter method and the value associated with the key is the name of the value you want to pass to that method
+# the ruby .send method calls the method name that is the key’s name, with an argument of the value
